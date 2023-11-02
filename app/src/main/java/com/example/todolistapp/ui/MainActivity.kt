c5bc889bc80442c8cf3c812bbcdd0fa650b49ef0
Tarik Safa Kutuk <tariksafakutuk1905@gmail.com>
2023-11-02 13:50:03 +0300

    :tada: Initial commit

---
+++ app/src/main/java/com/example/todolistapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.todolistapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.todolistapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
