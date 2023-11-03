be491aa1edd3f28a73614b24181bc9762228c1eb
Tarik Safa Kutuk <tariksafakutuk1905@gmail.com>
2023-11-03 18:35:36 +0300

    :lipstick: Add argument to navigation graph file

--- app/src/main/java/com/example/todolistapp/data/entity/Task.kt
+++ app/src/main/java/com/example/todolistapp/data/entity/Task.kt
@@ -1,8 +1,10 @@
 package com.example.todolistapp.data.entity
 
+import java.io.Serializable
+
 data class Task(var taskId: Int,
                 var taskTitle: String,
                 var taskDate: String,
                 var taskTime: String,
-                var taskActivated: Boolean) {
+                var taskActivated: Boolean) : Serializable {
 }
