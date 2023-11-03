f1c6a429aa0256d6f0a250dc27f9f1e92840ceaa
Tarik Safa Kutuk <tariksafakutuk1905@gmail.com>
2023-11-03 19:35:23 +0300

    :sparkles: Add design links to task detail fragment

--- app/src/main/java/com/example/todolistapp/ui/fragment/TaskDetailFragment.kt
+++ app/src/main/java/com/example/todolistapp/ui/fragment/TaskDetailFragment.kt
@@ -1,60 +1,55 @@
 package com.example.todolistapp.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.navArgs
 import com.example.todolistapp.R
+import com.example.todolistapp.databinding.FragmentTaskDetailBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TaskDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TaskDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentTaskDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_detail, container, false)
+    ): View {
+        requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.white)
+
+        binding = FragmentTaskDetailBinding.inflate(inflater, container, false)
+
+        binding.toolbarTaskDetail.title = "Task Detail"
+
+        binding.toolbarTaskDetail.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        val bundle: TaskDetailFragmentArgs by navArgs()
+        val task = bundle.task
+
+        binding.editTextTitle.setText(task.taskTitle)
+        binding.editTextDate.setText(task.taskDate)
+        binding.editTextTime.setText(task.taskTime)
+
+        binding.buttonUpdateTask.setOnClickListener {
+            val taskTitle = binding.editTextTitle.text.toString()
+            val taskDate = binding.editTextDate.text.toString()
+            val taskTime = binding.editTextTime.text.toString()
+            updateTask(task.taskId, taskTitle, taskDate, taskTime)
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TaskDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TaskDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    fun onBackPressed() {
+        requireActivity().onBackPressedDispatcher.onBackPressed()
+    }
+
+    fun updateTask(taskId: Int, taskTitle: String, taskDate: String, taskTime: String) {
+        Log.e("Message", "$taskId - $taskTitle - $taskDate - $taskTime")
     }
 }
