66f94efb987f6c24eceef0168e32522a3f8a862b
Tarik Safa Kutuk <tariksafakutuk1905@gmail.com>
2023-11-04 23:16:05 +0300

    :sparkles: Add data binding feature to add task fragment

--- app/src/main/java/com/example/todolistapp/ui/fragment/AddTaskFragment.kt
+++ app/src/main/java/com/example/todolistapp/ui/fragment/AddTaskFragment.kt
@@ -7,8 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import com.example.todolistapp.R
 import com.example.todolistapp.databinding.FragmentAddTaskBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class AddTaskFragment : Fragment() {
     private lateinit var binding: FragmentAddTaskBinding
@@ -19,26 +25,41 @@ class AddTaskFragment : Fragment() {
     ): View {
         requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.white)
 
-        binding = FragmentAddTaskBinding.inflate(inflater, container, false)
-
-        binding.toolbarAddTask.title = "Add Task"
-
-        binding.toolbarAddTask.setNavigationOnClickListener {
-            onBackPressed()
-        }
-
-        binding.buttonAddTask.setOnClickListener {
-            val taskTitle = binding.editTextTitle.text.toString()
-            val taskDate = binding.editTextDate.text.toString()
-            val taskTime = binding.editTextTime.text.toString()
-            createTask(taskTitle, taskDate, taskTime)
-        }
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_task, container, false)
+        binding.addTaskFragment = this
+        binding.addTaskToolbarTitle = "Add Task"
 
         return binding.root
     }
 
     fun onBackPressed() {
         requireActivity().onBackPressedDispatcher.onBackPressed()
+    }
+
+    fun selectDate() {
+        val datePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select Date")
+            .build()
+
+        datePicker.show(requireActivity().supportFragmentManager, "Date")
+
+        datePicker.addOnPositiveButtonClickListener {
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            binding.taskDate = dateFormat.format(it)
+        }
+    }
+
+    fun selectTime() {
+        val timePicker = MaterialTimePicker.Builder()
+            .setTitleText("Select Time")
+            .setTimeFormat(TimeFormat.CLOCK_24H)
+            .build()
+
+        timePicker.show(requireActivity().supportFragmentManager, "Time")
+
+        timePicker.addOnPositiveButtonClickListener {
+            binding.taskTime = "${timePicker.hour}:${timePicker.minute}"
+        }
     }
 
     fun createTask(taskTitle: String, taskDate: String, taskTime: String) {
