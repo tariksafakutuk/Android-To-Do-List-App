e68cb90efb8ae266ca2ecbe407ecfadc0075355f
Tarik Safa Kutuk <tariksafakutuk1905@gmail.com>
2023-11-04 23:37:09 +0300

    :sparkles: Add data binding feature to task detail fragment

--- app/src/main/java/com/example/todolistapp/ui/fragment/TaskDetailFragment.kt
+++ app/src/main/java/com/example/todolistapp/ui/fragment/TaskDetailFragment.kt
@@ -7,9 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.navArgs
 import com.example.todolistapp.R
 import com.example.todolistapp.databinding.FragmentTaskDetailBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class TaskDetailFragment : Fragment() {
     private lateinit var binding: FragmentTaskDetailBinding
@@ -20,33 +26,48 @@ class TaskDetailFragment : Fragment() {
     ): View {
         requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.white)
 
-        binding = FragmentTaskDetailBinding.inflate(inflater, container, false)
-
-        binding.toolbarTaskDetail.title = "Task Detail"
-
-        binding.toolbarTaskDetail.setNavigationOnClickListener {
-            onBackPressed()
-        }
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_task_detail, container, false)
+        binding.taskDetailFragment = this
+        binding.taskDetailToolbarTitle = "Task Detail"
 
         val bundle: TaskDetailFragmentArgs by navArgs()
         val task = bundle.task
 
-        binding.editTextTitle.setText(task.taskTitle)
-        binding.editTextDate.setText(task.taskDate)
-        binding.editTextTime.setText(task.taskTime)
-
-        binding.buttonUpdateTask.setOnClickListener {
-            val taskTitle = binding.editTextTitle.text.toString()
-            val taskDate = binding.editTextDate.text.toString()
-            val taskTime = binding.editTextTime.text.toString()
-            updateTask(task.taskId, taskTitle, taskDate, taskTime)
-        }
+        binding.taskObject = task
+        binding.taskDate = task.taskDate
+        binding.taskTime = task.taskTime
 
         return binding.root
     }
 
     fun onBackPressed() {
         requireActivity().onBackPressedDispatcher.onBackPressed()
+    }
+
+    fun selectDate() {
+        val datePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select Date")
+            .build()
+
+        datePicker.show(requireActivity().supportFragmentManager, "Date")
+
+        datePicker.addOnPositiveButtonClickListener {
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            binding.taskDate = dateFormat.format(it)
+        }
+    }
+
+    fun selectTime() {
+        val timePicker = MaterialTimePicker.Builder()
+            .setTitleText("Select Time")
+            .setTimeFormat(TimeFormat.CLOCK_24H)
+            .build()
+
+        timePicker.show(requireActivity().supportFragmentManager, "Time")
+
+        timePicker.addOnPositiveButtonClickListener {
+            binding.taskTime = "${timePicker.hour}:${timePicker.minute}"
+        }
     }
 
     fun updateTask(taskId: Int, taskTitle: String, taskDate: String, taskTime: String) {
