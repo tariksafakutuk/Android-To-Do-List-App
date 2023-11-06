c93c82b2e0e6c17b456d025747d4bd004eb008cb
tariksafakutuk <tariksafakutuk1905@gmail.com>
2023-11-06 14:29:22 +0300

    :heavy_plus_sign: Add ViewModel dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.activity:activity-ktx:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
